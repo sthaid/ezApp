9e27dedaae1a1b89d95e2d043ff08c34be839d51
Steven Haid <stevenhaid@gmail.com>
2026-07-30 08:05:57 -0400

    updates to cJSON

---
+++ src/cJSON/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := cJSON
+
+LOCAL_SRC_FILES := cJSON.c
+
+LOCAL_CFLAGS := -O2
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+
+include $(BUILD_STATIC_LIBRARY)
