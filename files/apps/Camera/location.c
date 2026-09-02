c61af7e6a2f33fba85d38ef18351e964a8e3b4b4
Steven Haid <stevenhaid@gmail.com>
2026-09-02 10:35:37 -0400

    cont

--- files/apps/Camera/location.c
+++ files/apps/Camera/location.c
@@ -1,8 +1,12 @@
+// xxx todo
+// - pan and pinch map
+
 #include "apps/Camera/common.h"
 
 #define DEG_TO_RAD (M_PI / 180.0)
 
-#define MAX_MAP 7
+#define MAX_MAPI 7
+#define MAX_MAPJ 5
 
 #define EVID_LOC  1000 //xxx move
 
@@ -22,9 +26,11 @@ double  longitude_nearest_city;
 char    city[100];
 char    state[100];
 double  w_miles;
-head_t  head[MAX_MAP][MAX_MAP];  // xxx global?
+double  h_miles;
+head_t  head[MAX_MAPI][MAX_MAPJ];  // xxx global?
 bool    del_mode = false;
 double y_top;
+int num_displayed_photos;
 
 void proc(void);
 void display_map(void);
@@ -43,6 +49,7 @@ void location(void)
     city[0] = '\0';
     state[0] = '\0';
     w_miles = 1;
+    h_miles = 0.7;
 
     y_top = 0;
     memset(head, 0, sizeof(head));
@@ -55,6 +62,7 @@ void location(void)
         sdlx_display_init(COLOR_BLACK, PORTRAIT);
 
         proc();
+        num_displayed_photos = 0;
         display_photos();
         display_map();
 
@@ -76,15 +84,15 @@ void location(void)
         reg_event_show_readme_file();
 
         int ctrls_h = 300;
-        reg_event_fill_rect(0, sdlx_win_height-ctrls_h-25, sdlx_win_width, ctrls_h+25, COLOR_BLACK, EVID_NOOP);
+        reg_event_fill_rect(0, sdlx_win_height-ctrls_h, sdlx_win_width, ctrls_h, COLOR_BLACK, EVID_NOOP);
 
-        y = sdlx_win_height - ctrls_h + (150 - sdlx_char_height_dflt) / 2;
+        y = sdlx_win_height - 250; //xxx make same in gallery
         reg_event_str(COL2X(0), y, COLOR_LIGHT_BLUE, "Home", EVID_HOME);
         reg_event_str(COL2X(7), y, COLOR_LIGHT_BLUE, "Up", EVID_PGUP);
         reg_event_str(COL2X(12), y, COLOR_LIGHT_BLUE, "Dn", EVID_PGDN);
         reg_event_str(COL2X(17), y, COLOR_LIGHT_BLUE, "End", EVID_END);
 
-        y += 150;
+        y += 130;
         reg_event_str(0, y, COLOR_LIGHT_BLUE, "Del", EVID_DEL);
         reg_event_str(sdlx_win_width-4*sdlx_char_width_dflt, y, COLOR_LIGHT_BLUE, "View", EVID_VIEW);
 
@@ -92,28 +100,26 @@ void location(void)
 
         sdlx_register_control_events(EVID_STG, "Stg", EVID_TAKE, TAKE, EVID_QUIT, "X");
 
-
-
         // present the display
         sdlx_display_present();
 
         // wait for event, with 1 sec timeout
-        sdlx_get_event(ONE_SEC, &event);
+        sdlx_get_event(5*ONE_SEC, &event);
         if (event.event_id == -1) {
             continue;
         }
 
         // process events
-        if (event.event_id >= EVID_LOC && event.event_id < EVID_LOC+(MAX_MAP*MAX_MAP)) {
+        if (event.event_id >= EVID_LOC && event.event_id < EVID_LOC+(MAX_MAPI*MAX_MAPJ)) {
             int i,j; //xxx check they are in range
             head_t *hd;
 
-            i = (event.event_id - EVID_LOC) / MAX_MAP;
-            j = (event.event_id - EVID_LOC) % MAX_MAP;
-            printf("I %s: got EVID_LOC %d %d\n", progname, i, j);
+            printf("GOT EVENT ID %d\n", event.event_id - EVID_LOC);
+            i = (event.event_id - EVID_LOC) / MAX_MAPI;
+            j = (event.event_id - EVID_LOC) % MAX_MAPI;
+            printf("GOT EVENT   %d %d\n", i,j);
             hd = &head[i][j];
             hd->selected = !hd->selected;
-            printf("set selected %d %d to %d\n", i, j, hd->selected);
         } else if (event.event_id >= EVID_SHOW_PHOTO && event.event_id < EVID_SHOW_PHOTO+max_photos) {
             int idx = event.event_id - EVID_SHOW_PHOTO; 
             show_photo(idx);
@@ -121,6 +127,8 @@ void location(void)
             int idx = event.event_id - EVID_DELETE_PHOTO;
             delete_photo(idx);
         } else {
+            int y_last;
+
             switch (event.event_id) {
             case EVID_QUIT:
                 end_program = true;
@@ -130,21 +138,24 @@ void location(void)
                 break;
             case EVID_TAKE:
                 take_photo();
-                //y_last = (max_photos == 0 ? 0 : ((max_photos + 1) / 2 - 1) * SPACING);
+                //y_last = (num_displayed_photos == 0 ? 0 : ((num_displayed_photos + 1) / 2 - 1) * SPACING);
                 //y_top = y_last - 2 * SPACING;
+                //y_top = 1e99;
                 break;
             case EVID_DEL:
                 del_mode = !del_mode;
                 break;
             case EVID_MOTION:
+                printf("MOTION y_top %0.3f\n", y_top);
                 y_top -= event.u.motion.yrel;
                 break;
             case EVID_HOME:
                 y_top = 0;
                 break;
             case EVID_END:
-                //y_last = (max_photos == 0 ? 0 : ((max_photos + 1) / 2 - 1) * SPACING);
+                //y_last = (num_displayed_photos == 0 ? 0 : ((num_displayed_photos + 1) / 2 - 1) * SPACING);
                 //y_top = y_last - 2 * SPACING;
+                y_top = 1e99;
                 break;
             case EVID_PGUP:
                 y_top -= (3 * SPACING);
@@ -162,9 +173,13 @@ void location(void)
             }
 
             // limit the min/max value of y_top 
-            //y_last = (max_photos == 0 ? 0 : ((max_photos + 1) / 2 - 1) * SPACING);
-            //if (y_top > y_last - 2 * SPACING) y_top = y_last - 2 * SPACING;
+            y_last = (num_displayed_photos == 0 
+                      ? 0 
+                      : ((num_displayed_photos + 1) / 2 - 1) * SPACING);
+            printf("num_displayed %d  y_last=%d\n", num_displayed_photos, y_last);
+            if (y_top > y_last - SPACING) y_top = y_last - SPACING;
             if (y_top < 0) y_top = 0;
+            printf("y_top is now %0.0f\n", y_top);
         }
     }
 }
@@ -176,7 +191,7 @@ void lat_long_to_map_pixel_coord(double latitude, double longitude, int *x, int 
          (w_miles / 2) * 1000 + 500;
     *y = (latitude_ctr - latitude) * 
          (69.17) / 
-         (w_miles / 2) * 1000 + 500;
+         (h_miles / 2) * 700 + 350;
     // xxx nearbyint
 }
 
@@ -198,8 +213,8 @@ void proc(void)
         printf("I %s: lat,long=%0.3f %0.3f  city,state=%s %s\n", progname, latitude_ctr, longitude_ctr, city, state);
     }
 
-    for (i = 0; i < MAX_MAP; i++) {
-        for (j = 0; j < MAX_MAP; j++) {
+    for (i = 0; i < MAX_MAPI; i++) {
+        for (j = 0; j < MAX_MAPJ; j++) {
             head[i][j].num_entries = 0;
             init_list_head(&head[i][j].node);
         }
@@ -211,10 +226,12 @@ void proc(void)
 
         lat_long_to_map_pixel_coord(md->latitude, md->longitude, &x, &y);
 
-        i = x / (1000.0 / MAX_MAP);
-        j = y / (1000.0 / MAX_MAP);
+        i = x / (1000.0 / MAX_MAPI);
+        j = y / (700.0 / MAX_MAPJ);
+
+        printf("%d %d  got i j %d %d\n", x,y,i,j);
         
-        if (i < 0 || i >= MAX_MAP || j < 0 || j >= MAX_MAP) {
+        if (i < 0 || i >= MAX_MAPI || j < 0 || j >= MAX_MAPJ) {
             continue;
         }
 
@@ -222,8 +239,8 @@ void proc(void)
         head[i][j].num_entries++;
     }
 
-    for (i = 0; i < MAX_MAP; i++) {
-        for (j = 0; j < MAX_MAP; j++) {
+    for (i = 0; i < MAX_MAPI; i++) {
+        for (j = 0; j < MAX_MAPJ; j++) {
             if (head[i][j].num_entries == 0){
                 head[i][j].selected = false;
             }
@@ -236,28 +253,29 @@ void display_map(void)
     int i, j, x, y;
     sdlx_loc_t loc;
 
-    sdlx_render_fill_rect(0, 0, 1000, 1000, COLOR_BLACK);
+    sdlx_render_fill_rect(0, 0, 1000, 700, COLOR_BLACK);
 
-    for (i = 0; i < MAX_MAP; i++) {
-        for (j = 0; j < MAX_MAP; j++) {
+    for (i = 0; i < MAX_MAPI; i++) {
+        for (j = 0; j < MAX_MAPJ; j++) {
             head_t *hd = &head[i][j];
 
             if (is_list_empty(&hd->node)) {
                 continue;
             }
 
-            loc.w = sdlx_win_width / MAX_MAP;
-            loc.h = loc.w;
+            loc.w = 1000.0 / MAX_MAPI;
+            loc.h = 700.0 / MAX_MAPJ;
             loc.x = i * loc.w;
             loc.y = j * loc.h;
             sdlx_color_t color = (hd->selected ? COLOR_ORANGE : COLOR_WHITE);
             sdlx_render_fill_rect(loc.x+10, loc.y+10, loc.w-20, loc.h-20, color);
-            sdlx_register_event(&loc, EVID_LOC + i*MAX_MAP + j);
+            printf("Registering %d\n", EVID_LOC + i*MAX_MAPI + j);
+            sdlx_register_event(&loc, EVID_LOC + i*MAX_MAPI + j);
         }
     }
 
     // xxx ctr if string not too long, else dont center
-    sdlx_render_printf_ex1(0, 1000-sdlx_char_height(FONT_SMALL), FONT_SMALL, COLOR_WHITE, "%s %s", city, state);
+    sdlx_render_printf_ex1(0, 700-sdlx_char_height(FONT_SMALL), FONT_SMALL, COLOR_WHITE, "%s %s", city, state);
 
     lat_long_to_map_pixel_coord(latitude_nearest_city, longitude_nearest_city, &x, &y);
     sdlx_render_point(x,y,COLOR_BLUE,MAX_POINT_SIZE);
@@ -270,18 +288,18 @@ void display_photos(void)
     int cnt = -1;
     sdlx_texture_t *t;
 
-    printf("display photos  %d\n", head[3][3].selected);
-
     t = sdlx_create_texture(THUMB, THUMB);
 
-    for (i = 0; i < MAX_MAP; i++) {
-        for (j = 0; j < MAX_MAP; j++) {
+    for (i = 0; i < MAX_MAPI; i++) {
+        for (j = 0; j < MAX_MAPJ; j++) {
             head_t *hd = &head[i][j];
 
             if (!hd->selected) {
                 continue;
             }
-            printf("selected %d %d  num_entries=%d\n", i, j, hd->num_entries);
+            //printf("selected %d %d  num_entries=%d\n", i, j, hd->num_entries);
+
+            num_displayed_photos += hd->num_entries;
 
             for (node = hd->node.next; node != &hd->node; node = node->next) {
                 photo_t *photo = (photo_t*)node;
@@ -293,17 +311,16 @@ void display_photos(void)
 
                 y = (cnt / 2) * SPACING;
                 if (y < y_top - SPACING) {
-                    printf("continue y=%d y_top=%0.0f\n", y, y_top);
                     continue;
                 }
-                if (y > y_top + 1000) {
-                    printf("break y=%d y_top=%0.0f\n", y, y_top);
+                if (y > y_top + 1300) {
                     break;
                 }
 
+
                 sdlx_set_texture_pixels(t, md->pixels);
                 dest.x = (cnt % 2) * SPACING;
-                dest.y = y - y_top + 1000;
+                dest.y = y - y_top + 700;
                 dest.w = THUMB;
                 dest.h = THUMB;
                 sdlx_render_texture(t, NULL, &dest);
