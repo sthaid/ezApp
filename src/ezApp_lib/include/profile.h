86c6e4a60779e841d77880728e9a5f935e985963
Steven Haid <stevenhaid@gmail.com>
2026-09-20 09:55:20 -0400

    add profile.h

---
+++ src/ezApp_lib/include/profile.h
@@ -0,0 +1,17 @@
+#ifndef __PROFILE_H__
+#define __PROFILE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// xxx comments/doc needed
+static inline int profile_start(void) { return 0; }
+static inline void profile_stop(int filter) {}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
